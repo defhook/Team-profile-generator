c86351cdc571ca81f4827b77b41e548f8ad0cee1
defhook <defhook152@gmail.com>
2022-04-03 21:36:29 -0700

    working generator

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-/*
-THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-WHEN I decide to finish building my team
-THEN I exit the application, and the HTML is generated
 
-*/
+
+
 const fs = require('fs');
 const inquirer = require('inquirer');
 const Employee = require('./lib/Employee');
@@ -13,6 +10,7 @@ const Intern = require('./lib/Intern');
 
 
 const teamArr = [];
+console.log("Welcome to the team generator!");
 
 function addManager () {
     inquirer.prompt([
@@ -47,7 +45,7 @@ function addManager () {
             name: 'email',
             message: "What is this manager's e-mail?",
             validate: emailInput => {
-                if (answer !== ""){
+                if (emailInput !== ""){
                     return true;
                 } else {
                     console.log("Please provide the manager's e-mail!")
@@ -60,7 +58,7 @@ function addManager () {
             name: 'officeNumber',
             message: "What is this manager's office number?",
             validate: officeNumberInput => {
-                if (answer !== ""){
+                if (officeNumberInput !== ""){
                     return true;
                 } else {
                     console.log("Please provide the manager's office number!")
@@ -191,17 +189,18 @@ function addIntern() {
             }
         },
     ]).then(answer => {
-        let intern = new Intern(answer.name, answer, id, answer.email, "Intern", answer.school);
+        let intern = new Intern(answer.name, answer.id, answer.email, "Intern", answer.school);
         teamArr.push(intern);
         createTeam();
     })
-};
+}
 
-const writeTofile = data => {
+const writeToFile = data => {
     const generatedHTML = generateHTML(data);
     return new Promise((resolve, reject) => {
-        fs.writeFile('./dist/generated.html', generateHTML, err=> {
+        fs.writeFile('./dist/generated.html', generatedHTML, err=> {
             if (err) {
+                reject(err);
             }
             resolve ({
                 ok: true,
@@ -211,24 +210,138 @@ const writeTofile = data => {
     });
 };
 
+function teamArrToHTML() {
+    const cardArr = teamArr.map(o => {
+        switch (o.role) {
+            case 'Manager':
+                return `
+                <div class="col-md-6 col-lg-4">
+                    <div class='card employee-card'>
+                        <div class='manager-card'>
+                            <div class='card-header bg-dark text-light'> 
+                                <h2><i class="fas fa-mug-hot"></i> Manager</h2>
+                            </div>
+                            <ul class="list-group list-group-flush list-unstyled">
+                                <li class=list-group-item"><h3>Name: ${o.getName()}</h3></li>
+                                <li class=list-group-item">Id: ${o.getId()}</li>
+                                <li class=list-group-item">Email: <a href="mailto:${o.getEmail()}">${o.getEmail()}</a></li>
+                                <li class=list-group-item">Office Number: ${o.getOffice()}</li>
+                            </ul>
+                        </div>
+                    </div> 
+                </div>
+                `
 
+            case 'Engineer':
+                return `
+                <div class="col-md=6 col-lg-4">
+                    <div class='card employee-card'>
+                        <div class='engineer-card'>
+                            <div class='card-header bg-dark text-light'>
+                                <h2><i class="fas fa-glasses"></i> Engineer</h2>
+                            </div>
+                            <ul class="list-group list-group-flush list-unstyled">
+                                <li class=list-group-item"><h3>Name: ${o.getName()}</h3></li>
+                                <li class=list-group-item">Id: ${o.getId()}</li>
+                                <li class=list-group-item">Email: <a href="mailto:${o.getEmail()}">${o.getEmail()}</a></li>
+                                <li class=list-group-item">Github: <a href="https://github.com/${o.getGithub()}" target="_blank">${o.getGithub()}</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div> 
+                `
 
-// function startPrompt() {
-//     console.log("Welcome to the team generator ");
-//     inquirer.prompt([
-//         {
-//             name: "list",
-//             type: "role",
-//             message: "What type of employee would you like to add?",
-//             choices: [
-//                 "Engineer",
-//                 "Intern",
-//                 "Manager",
-//                 "None"
-//             ]
-//         }, 
-        
-// }
+            case 'Intern':
+                return `
+                <div class="col-md-6 col-lg-4">
+                    <div class='card employee-card'>
+                        <div class='intern-card'>
+                            <div class='card-header bg-dark text-light'>
+                                <h2><i class="fas fa-user-graduate"></i> Intern</h2>
+                            </div>
+                            <ul class="list-group list-group-flush list-unstyled">
+                                <li class=list-group-item"><h3>Name: ${o.getName()}<h3></li>
+                                <li class=list-group-item">Id: ${o.getId()}</li>
+                                <li class=list-group-item">Email: <a href="mailto:${o.getEmail()}">${o.getEmail()}</a></li>
+                                <li class=list-group-item">School: ${o.getSchool()}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                `
+            default:
+                console.log('cardArr finished?')
+                return results
+        }
+    });
+    return cardArr;
+}
 
+const generateHTML = () => {
+    const results = `
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile Generator</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" />
+        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet" />
+    </head>
+    
+    <body>
+        <header>
+            <div class="container flex-row justify-space-between align-center py-3">
+            </div>
+        </header>
+        <main class="container my-5">
+            <div class="row">
+                ${teamArrToHTML().join('')}
+            </div>
+        </main>
+    </body>
+    </html>
+    `;
 
-//startPrompt();
+    return results;
+}
+
+function createTeam(){
+    inquirer.prompt([
+        {
+        type: "list",
+        name: "role",
+        message: "What type of employee would you like to add?",
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern",
+            "None"
+        ]
+        }
+    ]).then(answer => {
+        switch(answer.role){
+            case "Manager":
+                addManager();
+                break;
+
+            case "Engineer":
+                addEngineer();
+                break;
+
+            case "Intern":
+                addIntern();
+                break;
+
+            default:
+                console.log(teamArr);
+                writeToFile(teamArr); 
+        }; 
+    });
+
+}
+
+createTeam(); 
